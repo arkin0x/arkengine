f1141ff04bb66447ca1e81e1cbe45d94f0ac390b
arkin0x <arkinox@pm.me>
2023-08-11 18:10:46 -0500

    WIP intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,10 +1,12 @@
 import { SignInButton } from './SignInButton'
+import logo from '../assets/img/arkengine-b.jpg'
 import '../scss/Intro.scss'
 
 export const Intro = () => {
   return (
     <div id="component-intro">
       <div className="content">
+        <img src={logo} alt="logo" className="logo"/>
         <h1 className="title">Arkengine</h1>
         <h2 className="subtitle">borderless economic engine</h2>
         <div className="align-center">
